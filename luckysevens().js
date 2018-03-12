50dd9269fb69da99e5e7f82863bb00184421c644
blairkwagner <37027224+blairkwagner@users.noreply.github.com>
2018-03-11 21:42:38 -0500

    Lucky Seven

---
+++ luckysevens().js
@@ -0,0 +1,34 @@
+<div id="results">
+        <table>
+        <caption><h2>Results</h2></caption>
+            <tr>
+                <th>Starting Bet</th>
+                <th id="resultsBet"></th>
+            </tr>
+            <tr>
+                <td>Total Rolls Before Going Broke</td>
+                <td id="resultsRollCounter"></td>
+            </tr>
+            <tr>
+                <td>Highest Amount Won</td>
+                <td id="resultsHighestHeld"></td>
+            </tr>
+            <tr>
+                <td>Roll Count at Highest Amount Won</td>
+                <td id="resultsRollsFromHighest"></td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
